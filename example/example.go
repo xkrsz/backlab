d065d3cef98197fa2b0e742f9e408c452261975d
Krzysztof Kraszewski <krzysztof@kraszew.ski>
2018-05-18 21:14:02 +0200

    fix: change Init() to New()

--- example/example.go
+++ example/example.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"os"
 	"github.com/krszwsk/backlab"
+	"os"
 )
 
 func main() {
-	bl, _ := backlab.Init(backlab.Config{
+	bl, _ := backlab.New(backlab.Config{
 		Credentials: backlab.Credentials{
 			AccountID: os.Getenv("B2_ACCOUNT_ID"),
 			ApplicationKey: os.Getenv("B2_APPLICATION_KEY"),
